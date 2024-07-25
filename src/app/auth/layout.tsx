31c67fb9726e398e7edc5bf0271ce525b4c74907
Garima Sharma <sharmagarima814@gmail.com>
2024-07-25 22:42:32 +0530

    wip: layout.tsx with css

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -27,6 +27,27 @@ const Layout = async ({ children }: Props) => {
         />
         {children}
       </div>
+      <div className='hidden lg:flex flex-1 w-full max-h-full max-w-4000px overflow-hidden relative bg-cream flex-col pt-10 pl-24 gap-3'>
+        <h2 className='text-gravel md:text-4xl font-bold'>Hi, I am your AI powered sales assistant, Corinna!</h2>
+        <p>
+          Corinna is capable of capturing lead information without a form ... {''}
+          <br />
+          something never done before 😗
+        </p>
+        <Image
+          src='/images/app-ui.png'
+          alt='app image'
+          loading='lazy'
+          sizes='30'
+          className='absolute shrink-0 !w-[1600px] top-48'
+          style={{
+            width: '20%',
+            height: 'auto',
+          }}
+          height={0}
+          width={0}
+        />
+      </div>
     </div>
   );
 };
